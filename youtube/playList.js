bc5774a0b7558e0985cfde1ebd6e3192cd7e51f8
iris1411 <tmpss98212@gmail.com>
2022-04-14 00:26:00 +0800

    First commit

---
+++ youtube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "npPrO-xLnuE",
+    "gfse_qM9-u4",
+    "OMZZTrjWxt0"
+    ];
+    //播放起訖秒數
+    let playTime = [
+    [137,202],
+    [49,73],
+    [51,82]
+    ];
